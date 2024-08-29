ecf1c20b45e7ad7d814213cba22f9a70824935a6
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-30 00:20:48 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """filtred logger Module"""
+import logging
 from typing import List
 import re
 
@@ -16,3 +17,35 @@ def filter_datum(fields: List[str],
                            r'\1{}'.format(redaction),
                            obfus_msg)
     return obfus_msg
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """class constructor
+
+        Args:
+            fields (List[str]): list of fields
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """message format func
+
+        Args:
+            record (logging.LogRecord): log record
+
+        Returns:
+            str: obfuscated message
+        """
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
