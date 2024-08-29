b9181ed29ea3cf90fdb201525121d66298d097be
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-30 00:04:01 +0100

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 """filtred logger Module"""
+from typing import List
+import re
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """log message obfuscated"""
+    obfus_msg = message
+    for field in fields:
+        if field:
+            pattern = r'({}=)[^{}]+'.format(field, separator)
+            obfus_msg = re.sub(pattern,
+                               r'\1{}'.format(redaction),
+                               obfus_msg)
+    return obfus_msg
