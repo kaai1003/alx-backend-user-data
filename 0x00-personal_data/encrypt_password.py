a98493e6faefc316fe3188d9db70c4796fbb3b06
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-29 19:22:27 +0100

    filtered_logger.py

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""encrypt password Module"""
+from typing import List
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """log message obfuscated"""
+    pass
