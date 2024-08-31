1b79f592f8784c19c5d02314736f1291047ea9f9
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-01 00:46:37 +0100

    password bcrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 """encrypt password Module"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """return byte hashed password"""
+    return bcrypt.hashpw(password.encode('utf-8'),
+                         bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check password validation
+    """
+    return bcrypt.checkpw(password.encode('utf-8'),
+                          hashed_password)
