433cc105eef7e73e10daf3536bcb454c99306f73
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-03 19:42:18 +0100

    decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic authntefication Class module"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,17 @@ class BasicAuth(Auth):
         if authorization_header.startswith('Basic '):
             return authorization_header[len('Basic '):]
         return None
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str) -> str:
+        """base64 decode method"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            dec_str = base64.b64decode(base64_authorization_header)
+            return dec_str.decode('utf-8')
+        except Exception:
+            return None
