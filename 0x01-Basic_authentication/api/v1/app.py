dc8be408adde909a44824a4fbb40e5b854ab14eb
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-03 19:13:25 +0100

    add BasicAuth Class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,6 +14,9 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""basic authntefication Class module"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Basic Auth class definition"""
