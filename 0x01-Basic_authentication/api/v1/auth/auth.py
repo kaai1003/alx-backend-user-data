177b9e27ce2831bb4a3b6c76fad0dda9a27ea127
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-03 01:05:10 +0100

    task0 to task4

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""authntefication Class module"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class definition"""
+    def require_auth(self, path: str,
+                     excluded_paths: List[str]) -> bool:
+        """require auth method"""
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        for ex_path in excluded_paths:
+            if ex_path.rstrip('/') == path.rstrip('/'):
+                return False
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        """auth header method"""
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """curr user method"""
+        return None
