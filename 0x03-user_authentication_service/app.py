419e7eaa6c61dc8a278f2cce39cca7d7d15cccf9
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-13 00:52:53 +0100

    app.py

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""_summary_
+"""
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def welcome() -> str:
+    """GET welcome msg
+
+    Returns:
+        str: json payload
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
